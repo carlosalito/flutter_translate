e66ddd3a9645cb3b7a3271c887570ebecf026278
carlosalito <carlosalito@gmail.com>
2025-06-24 22:39:12 -0300

    remove base translate and after now need to use complete token key to refer translate value

--- lib/translate_mixin.dart
+++ lib/translate_mixin.dart
@@ -1,23 +1,14 @@
 part of 'translate.dart';
 
 mixin I18nTranslateMixin {
-  late final String _base;
-  late final String _dictId;
-
-  void setBaseTranslate({String? base, String? dictId}) {
-    _base = base ?? '';
-    _dictId = dictId ?? 'default';
-  }
-
   String translate(
     String key, {
-    bool useBaseTranslate = true,
     Map<String, String>? params,
     String? dictId,
   }) {
     return I18nTranslate.instance.translate(
-      '${useBaseTranslate ? '$_base.' : ''}$key',
-      dictId: dictId ?? _dictId,
+      key,
+      dictId: dictId ?? 'default',
       params: params,
     );
   }
